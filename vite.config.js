0f3647766746a32db76cae0f9b51f393abab4c82
asumanwebdev <asumanwebdev@gmail.com>
2022-06-13 22:39:09 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
-//import shopifyConfig from './shopify.config'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hydrogen(), netlifyPlugin()],
+  plugins: [hydrogen()],
   optimizeDeps: {include: ['@headlessui/react']},
   test: {
     globals: true,
